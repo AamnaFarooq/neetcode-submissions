cfc3829057d6f8020d075b159cb6f01f110dd3a3
AamnaFarooq <aamna.farooq9@gmail.com>
2026-08-07 14:18:20 -0400

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,81 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        
+        #searching in rows
+        for i in range(9):
+            seen = set()
+            for j in range(9):
+                if board[i][j] in seen and board[i][j] != '.':
+                    return False
+                seen.add(board[i][j] )
+
+
+        #searching in coloumns
+        for i in range(9):
+            seen = set()
+            for row in board:
+                for col in row[i]:
+                    if col in seen and col != '.':
+                        return False
+                    seen.add(col)  
+
+        #searching for 3x3 grid
+        seen1 = set()
+        seen2 = set()
+        seen3 = set()
+        for i in range(0,3):
+            for j in range(0,3):
+                if board[i][j] in seen1 and board[i][j] != '.':
+                    return False
+                seen1.add(board[i][j])
+  
+            for j in range(3,6):
+                if board[i][j] in seen2 and board[i][j] != '.':
+                    return False 
+                seen2.add(board[i][j])            
+
+            for j in range(6,9):
+                if board[i][j] in seen3 and board[i][j] != '.':
+                    return False  
+                seen3.add(board[i][j])           
+
+        seen1 = set()
+        seen2 = set()
+        seen3 = set()
+        for i in range(3,6):
+            for j in range(0,3):
+                if board[i][j] in seen1 and board[i][j] != '.':
+                    return False
+                seen1.add(board[i][j])
+
+            for j in range(3,6):
+                if board[i][j] in seen2 and board[i][j] != '.':
+                    return False
+                seen2.add(board[i][j])
+
+            for j in range(6,9):
+                if board[i][j] in seen3 and board[i][j] != '.':
+                    return False
+                seen3.add(board[i][j])
+
+        seen1 = set()
+        seen2 = set()
+        seen3 = set()
+        for i in range(6,9):
+            for j in range(0,3):
+                if board[i][j] in seen1 and board[i][j] != '.':
+                    return False
+                seen1.add(board[i][j])
+
+            for j in range(3,6):
+                if board[i][j] in seen2 and board[i][j] != '.':
+                    return False
+                seen2.add(board[i][j])
+
+            for j in range(6,9):
+                if board[i][j] in seen3 and board[i][j] != '.':
+                    return False
+                seen3.add(board[i][j])
+
+        return True
